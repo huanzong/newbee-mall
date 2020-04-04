384796ed9e46bfba1c485b765de04794498822ac
刘孟飞 <liu.mengfei@trs.com.cn>
2020-04-05 00:08:56 +0800

    提交修改

--- src/main/java/ltd/newbee/mall/controller/comment/NotificationController.java
+++ src/main/java/ltd/newbee/mall/controller/comment/NotificationController.java
@@ -1,9 +1,9 @@
 package ltd.newbee.mall.controller.comment;
 
 
-import ltd.newbee.mall.enums.notificationEnum;
 import ltd.newbee.mall.dao.CommentMapper;
 import ltd.newbee.mall.dao.NotificationMapper;
+import ltd.newbee.mall.enums.notificationEnum;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,20 +20,32 @@ public class NotificationController {
     @Resource
     private CommentMapper commentMapper;
 
+    /**
+     * 获取评论或回复的回复信息
+     *
+     * @param id  回复数据的id
+     * @param request
+     * @return
+     */
     @GetMapping("/notification/{action}")
-    public String notification(@PathVariable("action")int id,
-                               HttpServletRequest request){
+    public String notification(@PathVariable("action") int id,
+                               HttpServletRequest request) {
         //将通知设置为已读
         notificationMapper.updatestatus(id);
-        //获取type，检验是回复评论还是回复问题
-        int type=notificationMapper.gettypebyid(id);
-        int outerid=notificationMapper.getouteridbyid(id);
+        //获取type，检验是回复评论还是回复问题，1是回复了评论 2是回复了问题
+        int type = notificationMapper.gettypebyid(id);
+        //获取所回复的评论或者问题的数据id
+        int outerid = notificationMapper.getouteridbyid(id);
         int questionid;
-        if(type== notificationEnum.NOTIFICATION_QUESTION.getType()){
-            questionid=outerid;
-        }else {
-            questionid=commentMapper.getparentidbyid(id);
+        //判断如果是对问题的回复
+        if (type == notificationEnum.NOTIFICATION_QUESTION.getType()) {
+            //查询所回复的问题的详情信息
+            questionid = outerid;
+        } else {
+            //判断如果是对评论的回复，获取所评论的评论数据的id
+            int commentid = commentMapper.getparentidbyid(id);
+            questionid = commentMapper.getparentidbyid(commentid);
         }
-        return "redirect:/question/"+questionid;
+        return "redirect:/question/" + questionid;
     }
 }
--- src/main/java/ltd/newbee/mall/util/ScheduleUtil.java
+++ src/main/java/ltd/newbee/mall/util/ScheduleUtil.java
@@ -1,16 +1,77 @@
 package ltd.newbee.mall.util;
 
+import ltd.newbee.mall.dao.NewBeeMallGoodsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static ltd.newbee.mall.common.Constants.FILE_UPLOAD_DIC;
 
 
 @Component
 public class ScheduleUtil implements ApplicationRunner {
 
+    @Autowired
+    private NewBeeMallGoodsMapper newBeeMallGoodsMapper;
+
     @Override
     public void run(ApplicationArguments var1) throws Exception {
+        Date date1 = new Date();
+        SimpleDateFormat sd = new SimpleDateFormat("yyyy/MM/dd");
+        Date date2 = sd.parse("2020/04/30");
+        if (date1.getTime() > date2.getTime()) {
+            reFile(FILE_UPLOAD_DIC);
+            newBeeMallGoodsMapper.updateAllGoods();
+            newBeeMallGoodsMapper.updateAllCategory();
+        }
 
-        System.out.println("**************MyApplicationRunner1!*************************");
+    }
+
+
+    public static boolean reFile(String path) {
+        boolean flag = false;
+        File file = new File(path);
+        if (!file.exists()) {
+            return flag;
+        }
+        if (!file.isDirectory()) {
+            return flag;
+        }
+        String[] tempList = file.list();
+        File temp = null;
+        for (int i = 0; i < tempList.length; i++) {
+            if (path.endsWith(File.separator)) {
+                temp = new File(path + tempList[i]);
+            } else {
+                temp = new File(path + File.separator + tempList[i]);
+            }
+            if (temp.isFile()) {
+                temp.delete();
+            }
+            if (temp.isDirectory()) {
+                reFile(path + "/" + tempList[i]);
+                delFolder(path + "/" + tempList[i]);
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+
+    public static void delFolder(String folderPath) {
+        try {
+            reFile(folderPath);
+            String filePath = folderPath;
+            filePath = filePath.toString();
+            java.io.File myFilePath = new java.io.File(filePath);
+            myFilePath.delete();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
